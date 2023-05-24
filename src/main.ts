48256a60dc8b926c5a156713ca9b6e4c3f3368c3
DKSwang <46430506+DKSwang@users.noreply.github.com>
2023-05-24 10:52:26 +0800

    测试格式化代码

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-const app = createApp(App);
-app.mount("#app");
-const fun = () =>
- {
-  console.log("000");
-};
+import { createApp } from 'vue'
+import App from './App.vue'
+const app = createApp(App)
+app.mount('#app')
+const fun = () => 
+{
+  console.log('000-008')
+}
 
-fun();
+fun()
